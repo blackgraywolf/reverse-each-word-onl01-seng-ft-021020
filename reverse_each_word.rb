ae49df8850d9aa0d1a478e0a0b89a4c8cca53ffd
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-14 00:24:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
-def reverse_each_word(str)
- 
- str.reverse
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
 end
